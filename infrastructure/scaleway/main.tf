240cfd6b2e260234e97491a7707171c2f9cb4620
davidescus <rahthman.s@gmail.com>
2019-01-03 22:19:21 +0200

    Uncomment monitor machine

--- infrastructure/scaleway/main.tf
+++ infrastructure/scaleway/main.tf
@@ -82,55 +82,55 @@ output "Provision Machine Ip: " {
 }
 
 # --- monitor server(s)
-//resource "scaleway_ip" "ip-monitoring" {}
-//resource "scaleway_server" "server-monitoring" {
-//  name       = "server-monitoring"
-//  image      = "${var.os-image}"
-//  type       = "${var.instance-type}"
-//  public_ip  = "${scaleway_ip.ip-monitoring.ip}"
-//  depends_on = ["scaleway_ip.ip-monitoring", "scaleway_ip.ip-provision"]
-//  security_group = "${scaleway_security_group.monitoring.id}"
-//
-//  provisioner "remote-exec" {
-//    inline = [
-//      "apt-get update",
-//      "echo \"${scaleway_ip.ip-provision.ip}    salt\" >> /etc/hosts",
-//      "wget -O bootstrap-salt.sh https://bootstrap.saltstack.com",
-//      "sh bootstrap-salt.sh stable 2017.7",
-//    ]
-//
-//    connection {
-//      private_key = "${file("/home/davidescus/.ssh/id_rsa_decrypted")}"
-//    }
-//  }
-//}
-//
-//resource "scaleway_security_group" "monitoring" {
-//  name = "monitoring"
-//  description = "Security group for monitoring machines"
-//}
-//
-//resource "scaleway_security_group_rule" "monitoring-accept-prometheus-node-exporter" {
-//  security_group = "${scaleway_security_group.monitoring.id}"
-//  action    = "accept"
-//  direction = "inbound"
-//  ip_range  = "0.0.0.0/0"
-//  protocol  = "TCP"
-//  port      = 9100
-//}
-//
-//resource "scaleway_security_group_rule" "monitoring-accept-prometheus-web" {
-//  security_group = "${scaleway_security_group.monitoring.id}"
-//  action    = "accept"
-//  direction = "inbound"
-//  ip_range  = "0.0.0.0/0"
-//  protocol  = "TCP"
-//  port      = 9090
-//}
-//
-//output "Monitoring Machine Ip: " {
-//  value = "${scaleway_ip.ip-monitoring.ip}"
-//}
+resource "scaleway_ip" "ip-monitoring" {}
+resource "scaleway_server" "server-monitoring" {
+  name       = "server-monitoring"
+  image      = "${var.os-image}"
+  type       = "${var.instance-type}"
+  public_ip  = "${scaleway_ip.ip-monitoring.ip}"
+  depends_on = ["scaleway_ip.ip-monitoring", "scaleway_ip.ip-provision"]
+  security_group = "${scaleway_security_group.monitoring.id}"
+
+  provisioner "remote-exec" {
+    inline = [
+      "apt-get update",
+      "echo \"${scaleway_ip.ip-provision.ip}    salt\" >> /etc/hosts",
+      "wget -O bootstrap-salt.sh https://bootstrap.saltstack.com",
+      "sh bootstrap-salt.sh stable 2017.7",
+    ]
+
+    connection {
+      private_key = "${file("/home/davidescus/.ssh/id_rsa_decrypted")}"
+    }
+  }
+}
+
+resource "scaleway_security_group" "monitoring" {
+  name = "monitoring"
+  description = "Security group for monitoring machines"
+}
+
+resource "scaleway_security_group_rule" "monitoring-accept-prometheus-node-exporter" {
+  security_group = "${scaleway_security_group.monitoring.id}"
+  action    = "accept"
+  direction = "inbound"
+  ip_range  = "0.0.0.0/0"
+  protocol  = "TCP"
+  port      = 9100
+}
+
+resource "scaleway_security_group_rule" "monitoring-accept-prometheus-web" {
+  security_group = "${scaleway_security_group.monitoring.id}"
+  action    = "accept"
+  direction = "inbound"
+  ip_range  = "0.0.0.0/0"
+  protocol  = "TCP"
+  port      = 9090
+}
+
+output "Monitoring Machine Ip: " {
+  value = "${scaleway_ip.ip-monitoring.ip}"
+}
 
 # --- elk server(s)
 resource "scaleway_ip" "ip-elk" {}
